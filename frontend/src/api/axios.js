85db055575c25c2076322edff6c4e4a642cda0f3
NarenCK11 <cnkarthikeya@gmail.com>
2026-03-18 12:55:50 +0530

    backend path

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000",
+  baseURL: "https://ed-backend-mda9.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
